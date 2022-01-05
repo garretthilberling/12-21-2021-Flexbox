9f5b32b974a008859724f05e34b62b52e5874a82
garretthilberling <ghilberling@knights.ucf.edu>
2022-01-04 21:05:40 -0500

    in class javascript work

---
+++ 01.04.2022/02-Iteration/Unsolved/script.js
@@ -0,0 +1,8 @@
+// TODO: Write Your JavaScript Code Here
+var students = ('Diego', 'Olha', 'Jared', 'Garrett', 'Aaron');
+
+console.log(students.length);
+
+for (var i = 0; i < students.length; i++) {
+    console.log('Great to see you, ' + students(i) +'!')
+}
